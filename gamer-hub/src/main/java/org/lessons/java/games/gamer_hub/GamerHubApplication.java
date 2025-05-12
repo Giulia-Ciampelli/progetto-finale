5101b4c5853f79d3e8b9ead5f5a3880f6dc6adda
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-05-12 11:31:44 +0200

    initial commit

---
+++ gamer-hub/src/main/java/org/lessons/java/games/gamer_hub/GamerHubApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.java.games.gamer_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerHubApplication.class, args);
+	}
+
+}
---
+++ gamer-hub/src/test/java/org/lessons/java/games/gamer_hub/GamerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.games.gamer_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
