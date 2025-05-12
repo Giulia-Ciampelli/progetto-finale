8296829b320eb0edf4370aca55ee639cc3948b9a
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-05-12 13:07:08 +0200

    docs: user, roles and security configuration

---
+++ gamer-hub/src/main/java/org/lessons/java/games/gamer_hub/security/DatabaseUserDetailService.java
@@ -0,0 +1,29 @@
+package org.lessons.java.games.gamer_hub.security;
+
+import java.util.Optional;
+
+import org.lessons.java.games.gamer_hub.model.User;
+import org.lessons.java.games.gamer_hub.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseUserDetailService implements UserDetailsService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userAttempt = userRepository.findByUsername(username);
+
+        if (userAttempt.isEmpty()) {
+            throw new UsernameNotFoundException("Username '" + username + "' not found");
+        }
+
+        return new DatabaseUserDetails(userAttempt.get());
+    }
+}
---
+++ gamer-hub/src/main/java/org/lessons/java/games/gamer_hub/security/SecurityConfig.java
@@ -0,0 +1,61 @@
+package org.lessons.java.games.gamer_hub.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // gestione permessi singole pagine
+    @Bean
+    @SuppressWarnings("removal")
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .requestMatchers("/games/create", "/games/edit/**").hasAuthority("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/games/**").hasAuthority("ADMIN")
+                .requestMatchers("/games", "/games/**").hasAnyAuthority("USER", "ADMIN")
+
+                // TODO: aggiungi le altre pagine in seguito, o scleri come l'ultima volta
+                .requestMatchers("/**").permitAll()
+                .and().formLogin()
+                .and().logout()
+                .and().exceptionHandling()
+                .and().csrf().disable();
+
+        return http.build();
+    }
+
+    // gestione user details (provider)
+    @Bean
+    @SuppressWarnings("deprecation")
+    DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        // gestione servizio recupero users via username
+        authProvider.setUserDetailsService(userDetailService());
+
+        // gestione password encoder
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    DatabaseUserDetailService userDetailService() {
+        return new DatabaseUserDetailService();
+    }
+
+    // gestione password encoder
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
