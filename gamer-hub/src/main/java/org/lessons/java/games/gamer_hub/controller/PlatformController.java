e5d654de851ce09d2ff77152b6a2856444fb0c1d
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-05-12 21:06:25 +0200

    fix: missing web request

--- gamer-hub/src/main/java/org/lessons/java/games/gamer_hub/controller/PlatformController.java
+++ gamer-hub/src/main/java/org/lessons/java/games/gamer_hub/controller/PlatformController.java
@@ -13,11 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
 
 @Controller
+@RequestMapping("/platforms")
 public class PlatformController {
     
     @Autowired
